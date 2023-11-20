013925f6248524f10700c6aee995ed9eed7a8c0c
Sergey Zelenov <github@zelenov.su>
2023-11-20 18:47:28 +0100

    step3: add docker

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,14 +5,4 @@ module.exports = {
       script: "./index.js",
     },
   ],
-  deploy: {
-    production: {
-      user: "szelenov",
-      host: ["158.160.8.0"],
-      ref: "origin/main",
-      repo: "git@github.com:js-tasks-ru/nodejs-20230925-workshop.git",
-      path: "/home/szelenov/application",
-      "post-deploy": "npm install",
-    },
-  },
 };
